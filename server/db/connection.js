9e9ff23cda0e1d295a168e9aada0c484b51ef5d3
Prince Raj <rajprince031@gmail.com>
2024-09-30 00:13:20 +0530

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 async function connectMongoDB(url){
-    return mongoose.connect(url).then(()=>{console.log("MongoDB connected successfully")})
+    return await mongoose.connect(url).then(()=>{console.log("MongoDB connected successfully")})
     .catch((error)=>{console.log("error while connecting with MongoDB", error)})
 }
 
